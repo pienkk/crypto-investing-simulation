e8ce8942594a9a5fc009bc1c60deacc62bd38223
pienk <kisuk623@gmail.com>
2022-12-12 23:06:07 +0900

    [ADD] DB CONNECT

--- db/migrations/20221207064130_create_wallets_table.sql
+++ db/migrations/20221207064130_create_wallets_table.sql
@@ -2,9 +2,9 @@
 CREATE TABLE wallets(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   coinID INT NOT NULL,
-  userId INT NOT NULL
-  purchasePrice DECIMAL(12,4),
-  quantity DECIMAL(10,2),
+  userId INT NOT NULL,
+  purchasePrice DECIMAL(12,4) NOT NULL,
+  quantity DECIMAL(10,2) NOT NULL,
   CONSTRAINT wallets_coin_fkey FOREIGN KEY (coinId) REFERENCES coins (id),
   CONSTRAINT wallets_user_fkey FOREIGN KEY (userId) REFERENCES users (id)
 )
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MySqlConfigModule } from './config/database/config.module';
+import { MySqlConfigService } from './config/database/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev' ? './env/dev.env' : './env/test.env',
+      validationSchema: Joi.object({
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [MySqlConfigModule],
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
