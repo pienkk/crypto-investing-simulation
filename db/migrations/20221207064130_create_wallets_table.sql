d0531006490f2691022e930b5660b39f5f3e6d54
pienk <kisuk623@gmail.com>
2023-01-13 15:03:05 +0900

    [ADD] 랭킹정보 추가

--- db/migrations/20221207064130_create_wallets_table.sql
+++ db/migrations/20221207064130_create_wallets_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE wallets(
   purchasePrice DECIMAL(15,7) NOT NULL,
   quantity DECIMAL(15,7) NOT NULL,
   CONSTRAINT wallets_coin_fkey FOREIGN KEY (coinId) REFERENCES coins (id),
-  CONSTRAINT wallets_user_fkey FOREIGN KEY (userId) REFERENCES users (id)
+  CONSTRAINT wallets_user_fkey FOREIGN KEY (userId) REFERENCES users (id),
   CONSTRAINT wallets_coin_user_fkey UNIQUE (coinId,userId)
 )
 
